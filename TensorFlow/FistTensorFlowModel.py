70ba4ba650d54c40b058c67cb38778521ef46752
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-06-21 16:10:43 +0200

    update tensorflow model

---
+++ TensorFlow/FistTensorFlowModel.py
@@ -0,0 +1,21 @@
+# Hanwei Wang 21-06-2019
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)),
+    kersas
+
+])
