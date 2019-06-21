de7e484b53226b14c2597285143fa87823077978
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-06-21 11:12:55 +0200

    updates

--- secondNeturalWorl.py
+++ secondNeturalWorl.py
@@ -17,11 +17,37 @@ class Neuron():
         total = np.dot(self.weights, inputs) + self.bias
         return sigmod(total)
 
-weights = np.array([0, 1])
-bias = 4
-n = Neuron(weights, bias)
+class neuralNetwork():
+    """
+          A neural network with:
+        - 2 inputs
+        - a hidden layer with 2 neurons (h1, h2)
+        - an output layer with 1 neuron (o1)
+      Each neuron has the same weights and bias:
+        - w = [0, 1]
+        - b = 0
+    """
+    def __init__(self):
+        weights = np.array([0 , 1])
+        bias = 0
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
 
-x = np.array([2, 3])
-print(n.feedForward(x), 'in between 0 -- 1')
+    def feedForward(self, x):
+        out_h1 = self.h1.feedForward(x)
+        out_h2 = self.h2.feedForward(x)
+        # the inputs for 01 are the outputs from h1 and h2
+        out_o1 = self.o1.feedForward(np.array([out_h1, out_h2]))
+        return out_o1
+
+# <--- instantiation --->
+netWork = neuralNetwork()
+x = np.array([2, 3]) # inputs
+print(netWork.feedForward(x), 'in between 0 and 1')
+
+
+
+
 
 
